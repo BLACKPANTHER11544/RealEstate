d10e38eccbbbbb81152dc8cc5598310a9d5d0d9c
BLACKPANTHER11544 <vaibhavgiri460@gmail.com>
2024-01-11 16:03:35 +0530

    created Listing Page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/privateRoute";
+import CreateListing from "./pages/CreateListing";
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
         <Route path="/sign_up" element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create_Listing" element={<CreateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -22,6 +22,7 @@ import {
 } from "../redux/User/UserSlice";
 import { useDispatch } from "react-redux";
 import { errorHandler } from "../../../api/utilis/error";
+import { Link } from "react-router-dom";
 export default function Profile() {
   const dispatch = useDispatch();
   const fileRef = useRef(null);
@@ -184,6 +185,12 @@ export default function Profile() {
         >
           {loading ? "Loading" : "Update"}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95 "
+          to="/create_Listing"
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDelete} className="text-red-700 cursor-pointer">
